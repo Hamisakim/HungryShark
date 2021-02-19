c3ed202be8bb2a1622a80591f67422f1d17c0845
hamisakim <samishakim@gmail.com>
2021-02-19 01:13:09 +0000

    bruce swims up down left right

--- scripts/app.js
+++ scripts/app.js
@@ -43,11 +43,12 @@ function init() {
     const key = event.keyCode
     if (key === 39) {     //RIGHT
       cells[bruceCurrentPosition].classList.add('swim-right') // * flips right, but whole div
-      // } else if (key === 37 ) { //LEFT
-      //   bruceCurrentPosition--
-      // } else if (key === 38) {  //UP
-      //   bruceCurrentPosition -= width
-      // } else if (key === 40) { //DOWN
+    } else if (key === 37) { //LEFT
+      cells[bruceCurrentPosition].classList.remove('swim-right')
+    } else if (key === 38) {
+      cells[bruceCurrentPosition].classList.add('swim-up')//UP
+    } else if (key === 40) { //DOWN
+      cells[bruceCurrentPosition].classList.add('swim-down')
       //   bruceCurrentPosition += width
       // } 
 
