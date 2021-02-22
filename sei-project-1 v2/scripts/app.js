45b01127ff4dfee2a4d733722ae66c6c9ca68e61
hamisakim <samishakim@gmail.com>
2021-02-22 14:22:16 +0000

    walls and cage array works

--- sei-project-1 v2/scripts/app.js
+++ sei-project-1 v2/scripts/app.js
@@ -13,6 +13,8 @@ function init() {
     currentPosition: 90
   }
 
+  const bruceHistory = []
+
   class Enemy {
     constructor(name, startPosition) {
       this.name = name
@@ -23,10 +25,18 @@ function init() {
   }
 
   const enemies = [
+    new Enemy('quint', 150),
     new Enemy('shamu', 288),
     new Enemy('squid', 89),
     new Enemy('flipper', 190)
   ]
+
+   const cage = 
+    [229,230,267,268,269,270,271,272,287,287,288,288,289,289,290,290,291,291,292,292]
+  
+
+  const walls = [29,30,42,44,45,46,47,49,50,52,53,54,55,57,64,65,66,67,69,70,72,73,74,75,82,84,85,86,87,89,90,92,93,94,95,97,102,117,122,124,125,126,127,128,129,130,131,132,133,134,135,137,149,150,162,164,165,166,167,169,170,172,173,174,175,177,182,184,185,186,187,189,190,192,193,194,195,197,221,222,224,226,227,228,231,232,233,235,237,238,241,242,244,246,253,255,257,258,266,273,282,283,284,286,293,295,296,297,302,303,304,306,307,308,309,310,311,312,313,315,316,317,322,323,324,335,336,337,346,347,349,350,352,353,361,363,364,366,367,369,370,372,373,375,376,378,381,383,384,386,387,389,390,392,393,395,396,398,409,410,422,423,424,425,427,428,429,430,431,432,434,435,436,437,442,443,444,445,447,448,449,450,451,452,454,455,456,457]
+  
 
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
@@ -36,64 +46,80 @@ function init() {
       gameGrid.appendChild(cell)
       cellArray.push(cell)
 
-      if ((i % 20 === 0) || (i > 480) || (i < 20) || ((i + 1) % 20 === 0) || (i === 167) || (i === 168) || (i === 187) || (i === 207) || (i === 227) || (i === 229) || (i === 230) || (i === 231) || (i === 232) || (i === 212) || (i === 192) || (i === 172) || (i === 171) || (i === 228)) {
+      if ((i % 20 === 0) || (i > 480) || (i < 20) || ((i + 1) % 20 === 0)) {
+        cell.textContent = 'X'
+        cell.classList.add('wall')
+      } 
+      else if ( walls.includes(i) === true){
         cell.textContent = 'X'
         cell.classList.add('wall')
       }
+      else if (cage.includes(i) === true) {
+        cell.classList.add('cage')
+      }
+
     }
     addBruce(bruce.startPosition)
+    addEnemies()
+
   }
   createGrid()
-
+  moveEnemy()
+  function addEnemies() {
+    enemies.forEach(enemy => {
+      //console.log('ENEMY', enemy)
+      cellArray[enemy.startPosition].classList.add(enemy.class, enemy.name)
+      //cellArray[enemy.startPosition].classList.add()
+    })
+  }
 
   function addBruce(position) {
     cellArray[position].classList.add(bruce.class)
     // console.log('cellArray position', cellArray[position])
   }
 
-  enemies.forEach(enemy => {
-    //console.log('ENEMY', enemy)
-    cellArray[enemy.startPosition].classList.add(enemy.class, enemy.name)
-    //cellArray[enemy.startPosition].classList.add()
-  })
+
 
   function moveBruce(event) {
     const key = event.keyCode
     cellArray[bruce.currentPosition].classList.remove(bruce.class)
 
-    console.log('CURRENT', bruce.currentPosition)
+    //console.log('CURRENT', bruce.currentPosition)
     if ((key === 39 || key === 68) && (!cellArray[bruce.currentPosition + 1].classList.contains('wall'))) {
       bruce.currentPosition++
-    } else if ((key === 37 || key === 65) && (!cellArray[bruce.currentPosition - 1].classList.contains('wall'))) { //Left
+    } else if ((key === 37 || key === 65) && (!cellArray[bruce.currentPosition - 1].classList.contains('wall')) && (!cellArray[bruce.currentPosition - 1].classList.contains('cage'))) { //Left
       bruce.currentPosition--
     } else if ((key === 38 || key === 87) && (!cellArray[bruce.currentPosition - width].classList.contains('wall'))) { //UP
       bruce.currentPosition -= width
-    } else if ((key === 40 || key === 83) && (!cellArray[bruce.currentPosition + width].classList.contains('wall'))) { //down
+    } else if ((key === 40 || key === 83) && (!cellArray[bruce.currentPosition + width].classList.contains('wall')) && (!cellArray[bruce.currentPosition + width].classList.contains('cage'))) { //down
 
       bruce.currentPosition += width
     }
     addBruce(bruce.currentPosition)
-    moveEnemy()
+    bruceHistory.push(bruce.currentPosition)
+    //console.log(bruceHistory)
+
   }
+
 
 
   function moveEnemy() {
     const enemyTimer = setInterval(() => {
-    enemies.forEach(enemy => {
-      const randomIndex = Math.floor(Math.random() * 4)
-      cellArray[enemy.currentPosition].classList.remove(enemy.class, enemy.name)
-      if ((randomIndex === 0) && (!cellArray[enemy.currentPosition + 1].classList.contains('wall'))) {
-        enemy.currentPosition++
-      } else if ((randomIndex === 1) && (!cellArray[enemy.currentPosition - 1].classList.contains('wall'))) {
-        enemy.currentPosition--
-      } else if ((randomIndex === 2) && (!cellArray[enemy.currentPosition - width].classList.contains('wall'))) {
-        enemy.currentPosition -= width
-      } else if ((randomIndex === 3) && (!cellArray[enemy.currentPosition + width].classList.contains('wall'))) {
-        enemy.currentPosition += width
-      }
-      cellArray[enemy.currentPosition].classList.add(enemy.class, enemy.name)
-    })
-  },500)
+      enemies.forEach(enemy => {
+        const randomIndex = Math.floor(Math.random() * 4)
+        cellArray[enemy.currentPosition].classList.remove(enemy.class, enemy.name)
+        if ((randomIndex === 0) && (!cellArray[enemy.currentPosition + 1].classList.contains('wall'))) {
+          enemy.currentPosition++
+        } else if ((randomIndex === 1) && (!cellArray[enemy.currentPosition - 1].classList.contains('wall'))) {
+          enemy.currentPosition--
+        } else if ((randomIndex === 2) && (!cellArray[enemy.currentPosition - width].classList.contains('wall'))) {
+          enemy.currentPosition -= width
+        } else if ((randomIndex === 3) && (!cellArray[enemy.currentPosition + width].classList.contains('wall'))) {
+          enemy.currentPosition += width
+        }
+        cellArray[enemy.currentPosition].classList.add(enemy.class, enemy.name)
+      })
+    }, 300)
   }
 
 
--- sei-project-1 v2/scripts/app2.js
+++ sei-project-1 v2/scripts/app2.js
@@ -1,73 +1,79 @@
-/* eslint-disable indent */
-/* eslint-disable no-unused-vars */
-function init() {
+// /* eslint-disable indent */
+// /* eslint-disable no-unused-vars */
+// function init() {
 
-  const gameGrid = document.querySelector('.game-grid')
-  const width = 28
-  const height = 35
-  const cellCount = width * height
-  const cellArray = []
+//   const gameGrid = document.querySelector('.game-grid')
+//   const width = 28
+//   const height = 35
+//   const cellCount = width * height
+//   const cellArray = []
 
-  function createGrid() {
-    for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      cell.classList.add('square')
-      cell.textContent = i
-      gameGrid.appendChild(cell)
-      cellArray.push(cell)
-    }
-  }
-  createGrid()
+//   function createGrid() {
+//     for (let i = 0; i < cellCount; i++) {
+//       const cell = document.createElement('div')
+//       cell.classList.add('square')
+//       cell.textContent = i
+//       gameGrid.appendChild(cell)
+//       cellArray.push(cell)
+//     }
+//   }
+//   createGrid()
 
-  class Characters {
-    constructor(name, startPosition) {
-      this.name = name
-      this.startPosition = startPosition
-    }
-    spawn(startPosition, name) {
-      name = this.constructor.name
-      console.log('CLASS NAME', name)
-      console.log('CELL ARRAY ', cellArray[startPosition])
-    }
-  }
-
-
-  class Enemy extends Characters {
-    constructor(name, startPosition) {
-      super(name, startPosition)
-    }
-  }
-
-  class Player extends Characters {
-    constructor(name, startPosition) {
-      super(name)
-      this.startPosition = startPosition
-    }
-  }
-
-  const bruce = new Player('bruce', 0)
-  //console.log(bruce.name)
-
-  const shamu = new Enemy('shamu', 100)
-
-  bruce.spawn(10)
-  shamu.spawn(10)
+//   class Characters {
+//     constructor(name, startPosition) {
+//       this.name = name
+//       this.startPosition = startPosition
+//     }
+//     spawn(startPosition, name) {
+//       name = this.constructor.name
+//       console.log('CLASS NAME', name)
+//       console.log('CELL ARRAY ', cellArray[startPosition])
+//     }
+//   }
 
 
+//   class Enemy extends Characters {
+//     constructor(name, startPosition) {
+//       super(name, startPosition)
+//     }
+//   }
 
+//   class Player extends Characters {
+//     constructor(name, startPosition) {
+//       super(name)
+//       this.startPosition = startPosition
+//     }
+//   }
 
-  //shamu.spawn()
+//   const bruce = new Player('bruce', 0)
+//   //console.log(bruce.name)
 
-  // console.log(shamu.name,shamu.startPosition)
-  // console.log(bruce.name,bruce.startPosition)
+//   const shamu = new Enemy('shamu', 100)
 
-
-
-  //? BRACKET MUST BE YELLOW BELOW /////
-}
-window.addEventListener('DOMContentLoaded', init)
+//   bruce.spawn(10)
+//   shamu.spawn(10)
 
 
 
 
-  || (i === 167) || (i === 169) || (i === 187) || (i === 207) || (i === 227) || (i === 229) || (i === 230) || (i === 231) || (i === 232) || (i === 212) || (i === 192) || (i === 172) || (i === 171)
+//   //shamu.spawn()
+
+//   // console.log(shamu.name,shamu.startPosition)
+//   // console.log(bruce.name,bruce.startPosition)
+
+
+
+//   //? BRACKET MUST BE YELLOW BELOW /////
+// }
+// window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+//   || (i === 167) || (i === 169) || (i === 187) || (i === 207) || (i === 227) || (i === 229) || (i === 230) || (i === 231) || (i === 232) || (i === 212) || (i === 192) || (i === 172) || (i === 171)
+
+//   || (i === 170) || (i === 188) || (i === 189) || (i === 190) || (i === 191) || (i === 208) || (i === 209) || (i === 210)
+
+const layout = [
+  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+  console.log(layout.length)
