6a810ac9ece18e76c6eaeaa74a2bc176e352029c
hamisakim <samishakim@gmail.com>
2021-02-23 16:23:22 +0000

    POWER UP FUCNTION WOKRSgit add .git add .git add . :D:D:D:D:Dgit add .!

--- sei-project-1 v2/scripts/app2.js
+++ sei-project-1 v2/scripts/app2.js
@@ -1,33 +1,17 @@
-/* eslint-disable no-unused-vars */
-function moveEnemyRight() {
-  if (!cellArray[enemy.currentPosition + 1].classList.contains('wall')) {
-    cellArray[enemy.currentPosition + 1].classList.add('swim-right')
-    cellArray[enemy.currentPosition].classList.remove('swim-left', 'swim-up', 'swim-down', 'swim-right')
-    enemy.currentPosition++
+
+function moveRight(character) {
+  let position = character.currentPosition
+  console.log(position)
+  console.log('MOVE RIGHT FN')
+
+  do{  
+
+    console.log('MOVE')
+    cellArray[position + 1].classList.add('swim-right')
+    cellArray[position].classList.remove('swim-left', 'swim-up', 'swim-down', 'swim-right')
+    character.currentPosition++
   }
-
-}
-
-function moveEnemyLeft() {
-  if (!cellArray[enemy.currentPosition - 1].classList.contains('wall')) {
-    cellArray[enemy.currentPosition - 1].classList.add('swim-left')
-    cellArray[enemy.currentPosition].classList.remove('swim-right', 'swim-up', 'swim-down', 'swim-left')
-    enemy.currentPosition--
-  }
-
-
-  function moveEnemyUp(enemy) {
-    if (!cellArray[enemy.currentPosition - width].classList.contains('wall')) {
-      cellArray[enemy.currentPosition - width].classList.add('swim-up')
-      cellArray[enemy.currentPosition].classList.remove('swim-left', 'swim-up', 'swim-down', 'swim-right')
-      enemy.currentPosition -= width
-    }
-  }
-
-
-  function moveEnemy(enemy){
-if (!cellArray[enemy.currentPosition + width].classList.contains('wall')) {
-    cellArray[enemy.currentPosition + width].classList.add('swim-down')
-    cellArray[enemy.currentPosition].classList.remove('swim-left', 'swim-up', 'swim-down', 'swim-right')
-    enemy.currentPosition += width
+ 
+  while (!cellArray[position + 1].classList.contains('wall')) {
+  
 }
